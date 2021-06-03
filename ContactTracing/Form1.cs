7d71cf6cdeb985f4dff7d8c7a02f378697f768cc
Jenner <Jenner_defiesta@yahoo.com>
2021-06-03 18:00:55 +0800

    check error cbox dtime

--- ContactTracing/Form1.cs
+++ ContactTracing/Form1.cs
@@ -36,9 +36,40 @@ namespace ContactTracing
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (cobx_brgy.SelectedIndex > -1 && cobx_Cstatus.SelectedIndex > -1)
+            DateTime limit = new DateTime(1899,12,31);
+            
+            if (date_birth.Value > DateTime.Today || date_birth.Value <= limit)
             {
-
+                label_birth.Text = "Invalid Input";
+            }
+            else
+            {
+                label_birth.Text = "Corek";
+            }
+            //label_birth.Text = DateTime.Today.ToString() ;
+            if (cobx_brgy.SelectedIndex < 0)
+            {
+                label_brgy.Text = "Invalid input";
+            }
+            else
+            {
+                label_brgy.Text = "corek";
+            }
+            if (cobx_Cstatus.SelectedIndex < 0)
+            {
+                label_Cstatus.Text = "Invalid input";
+            }
+            else
+            {
+                label_Cstatus.Text = "corek";
+            }
+            if (!(radio_female.Checked || radio_male.Checked))
+            {
+                label_sex.Text = "Invalid input";
+            }
+            else
+            {
+                label_sex.Text = "corek";
             }
         }
     }
